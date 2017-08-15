6a775a347c5d9a97edf4718bba8d5d8e90a4b6d5
eslamtayseer <eslam_tayseer@yahoo.com>
2017-08-15 19:25:22 +0200

    added rottion and attempt to create front vector
    Former-commit-id: bc9d21692142bc7b032d002d1b1a40b100c90e16
    Former-commit-id: 1aa75667dff9d45edd4e6f4e73d59190ce739864
    Former-commit-id: 1f6f68172bca920f6fe0a29ea6179598138f84c0

--- js/game.js
+++ js/game.js
@@ -16,23 +16,26 @@ var isDPressed = false;
 var isWPressed = false;
 var isLeftPressed = false;
 var isRightPressed = false;
+var isUpPressed = false;
+var isDownPressed = false;
+
 
 function startGame() {
-	canvas = document.getElementById("renderCanvas");
-	engine = new BABYLON.Engine(canvas);
-	currentLevel = 0;
-	loadScene();
+    canvas = document.getElementById("renderCanvas");
+    engine = new BABYLON.Engine(canvas);
+    currentLevel = 0;
+    loadScene();
      
      
-	engine.runRenderLoop(function() {
-		if(scene) {
-			scene.render();
-			if(dragon) {
-				applyMovement();
-			}
-		}
-	});
-
+    engine.runRenderLoop(function() {
+        if(scene) {
+            scene.render();
+            if(dragon) {
+                applyMovement();
+            }
+        }
+    });
+}
     document.addEventListener("keydown", function (event) {
 
         if (event.key == 'a' || event.key == 'A') {
@@ -48,7 +51,7 @@ function startGame() {
             console.log(" W true");
 
         }
-        if (event.key == '37') {
+      /*  if (event.key == '37') {
             isLeftPressed = true;
             
             console.log(" left true");
@@ -58,27 +61,94 @@ function startGame() {
             console.log(" right true");
 
 
-        }
+        }*/
         
 
 
     });
+    document.onkeydown = function (e) {
+        if (e.keyCode == 37) {
+            isLeftPressed = true;
+
+           // console.log(" left true");
+        }
+        if (e.keyCode == 39) {
+            isRightPressed = true;
+            // console.log(" right true");
+        }
+            if (e.keyCode == 38) {
+                isUpPressed = true;
+
+              //  console.log(" left true");
+            }
+            if (e.keyCode == 40) {
+                isDownPressed = true;
+               // console.log(" right true");
+
+        }
+
+
+    }
+    document.onkeyup = function (e) {
+        if (e.keyCode == 37) {
+            isLeftPressed = false;
+
+           // console.log(" left true");
+        }
+        if (e.keyCode == 39) {
+            isRightPressed = false;
+           // console.log(" right true");
+
+
+        }
+
+        if (e.keyCode == 38) {
+            isUpPressed = false;
+
+            //  console.log(" left true");
+        }
+        if (e.keyCode == 40) {
+            isDownPressed = false;
+            // console.log(" right true");
+
+        }
+
+    }
+    /*document.addEventListener("onkeydown", function (event) {
+        console.log(event.keyCode);
+
+        
+        if (event.keyCode == 37) {
+            isLeftPressed = true;
+            
+            console.log(" left true");
+        }
+        if (event.keyCode == 39) {
+            isRightPressed = true;
+            console.log(" right true");
+
+
+        }
+
+
+
+    });*/
 
     document.addEventListener("keyup", function () {
 
         if (event.key == 'a' || event.key == 'A') {
             isAPressed = false;
-            console.log(" A false");
+           // console.log(" A false");
         }
         if (event.key == 'd' || event.key == 'D') {
             isDPressed = false;
-              console.log(" d false");
+           //   console.log(" d false");
         }
         if (event.key == 'w' || event.key == 'W') {
             isWPressed = false;
-              console.log(" w false");
+           //   console.log(" w false");
         }
-       if (event.key == '37') {
+       /*if (event.key == '37') {
             isLeftPressed = false;
 
             console.log(" left false");
@@ -87,11 +157,11 @@ function startGame() {
             isRightPressed = false;
 
             console.log(" left false");
-        }
+        }*/
         // breath fire
 
        
-    });}
+    });
 
 
 
@@ -122,8 +192,10 @@ function createDragon() {
 
 	function onDragonLoaded(newMeshes, particleSystems, skeletons) {
 	 	dragon = newMeshes[1];
-	 	dragon.position = new BABYLON.Vector3(0, 30, 0);
-        dragon.scaling = new BABYLON.Vector3(3, 3, 3);
+	 	dragon.position = new BABYLON.Vector3(0, 30, -1);
+	 	dragon.scaling = new BABYLON.Vector3(3, 3, 3);
+	 	dragon.frontVector = new BABYLON.Vector3(0, 0, -1);
+	 	dragon.rotation.y = 3.14;
         camera.lockedTarget = dragon;
         camera.heightOffset = 20;
         camera.radius = 60;
@@ -135,21 +207,48 @@ function createDragon() {
     	
     	if(isAPressed)
     	{
-    		dragon.position.x += 1;
+    	    dragon.position.x += -1;
+    	   // console.log("x :" + dragon.position.x)
+
     	}
     	if (isDPressed) {
-    		dragon.position.x += -1;
+    	    dragon.position.x += 1;
+    	    //console.log("x :" + dragon.position.x)
+
     	}
        if (isWPressed) {
-             dragon.position.z += -1;
+           //  dragon.position.z += -1;
+           // dragon.moveWithCollisions(dragon.frontVector.multiplyByFloats(1, 1, 1));
+
+           //dragon.frontVector.z--;
+         //  console.log(dragon.frontVector.z * Math.cos(dragon.rotation.y)*-1 );
+          // console.log(dragon.frontVector.z * Math.cos(dragon.rotation.y) * -1);
+           dragon.position.x +=  dragon.frontVector.x /* * Math.cos(dragon.rotation.y) */ *-1;
+           dragon.position.z += dragon.frontVector.z /* * Math.cos(dragon.rotation.y)*/  * -1;
+           console.log("x :" + dragon.position.x + "       Z:" + dragon.position.z)
+           console.log("Front vector x :" + dragon.frontVector.x + "       Z:" + dragon.frontVector.z)
+
+
        }
        if (isLeftPressed)
        {
-           dragon.rotation.y -= .1 *0.4 ;
+           dragon.rotation.y -= .1 * 0.4;
+           dragon.frontVector.x  =  Math.sin(dragon.rotation.y) * -1;
+           dragon.frontVector.z  = Math.cos(dragon.rotation.y) * -1;
        }
        if (isRightPressed)
        {
            dragon.rotation.y += .1 * 0.4;
+           dragon.frontVector.x = Math.sin(dragon.rotation.y) * -1;
+           dragon.frontVector.z = Math.cos(dragon.rotation.y) * -1;
+       }
+       if (isUpPressed)
+       {
+           dragon.position.y += 1 ;
+       }
+       if (isDownPressed)
+       {
+           dragon.position.y -= 1;
        }
        if (dragon.position.x>=500  )
            dragon.position.x = 500
@@ -159,7 +258,8 @@ function createDragon() {
            dragon.position.z = 500
        if (dragon.position.z <= -475)
            dragon.position.z = -475
-
+     //  dragon.frontVector.x = Math.sin(dragon.rotation.y) * -1;
+        // dragon.frontVector.z = Math.cos(dragon.rotation.y) * -1;
     }
 
 
@@ -189,10 +289,10 @@ function createConfiguredGround()
         50,-50, 100, scene, false, onGroundCreated);
 
     var groundMaterial = new BABYLON.StandardMaterial("m1", scene);
-   // groundMaterial.ambientColor = new BABYLON.Color3(1, 0, 0);
-   // groundMaterial.diffuseColor = new BABYLON.Color3(1, 0, 0);
+    // groundMaterial.ambientColor = new BABYLON.Color3(1, 0, 0);
+    // groundMaterial.diffuseColor = new BABYLON.Color3(1, 0, 0);
     groundMaterial.diffuseTexture = new BABYLON.Texture("scenes/RockMountain.jpg", scene);
-   groundMaterial.diffuseTexture.uScale = 10;
+    groundMaterial.diffuseTexture.uScale = 10;
     groundMaterial.diffuseTexture.vScale = 10;
 
     function onGroundCreated() {
