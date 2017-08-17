bdc67e48949a258c637531cb53cf0f00ec3f506c
MahmoudKhalil <mahmoudkhalil11@gmail.com>
2017-08-17 16:52:23 +0200

    Replaced dragon with the box until the bug gets fixed
    Former-commit-id: 6c130b56a9e839ac2aa842ea853e2b04ac77ae92
    Former-commit-id: 2a3e5d7b424747a04b58881c6a1717be42a94406
    Former-commit-id: 7c4208fa4a09b81da05e0e5511e2f4998697e866

--- js/game.js
+++ js/game.js
@@ -27,7 +27,7 @@ function startGame() {
     currentLevel = 0;
     loadScene();
     loadCoins();
-     
+
     engine.runRenderLoop(function() {
         if(scene) {
             scene.render();
@@ -37,296 +37,255 @@ function startGame() {
         }
     });
 }
-    document.addEventListener("keydown", function (event) {
+document.addEventListener("keydown", function (event) {
 
-        if (event.key == 'a' || event.key == 'A') {
-            isAPressed = true;
-            console.log(" A true");
-        }
-        if (event.key == 'd' || event.key == 'D') {
-            isDPressed = true;
-            console.log(" D true");
-        }
-        if (event.key == 'w' || event.key == 'W') {
-            isWPressed = true;
-            console.log(" W true");
+    if (event.key == 'a' || event.key == 'A') {
+        isAPressed = true;
+    }
 
-        }
-        if (event.key == 's' || event.key == 'S') {
-            isSPressed = true;
-            console.log(" S true");
+    if (event.key == 'd' || event.key == 'D') {
+        isDPressed = true;
+    }
 
-        }
+    if (event.key == 'w' || event.key == 'W') {
+        isWPressed = true;
+    }
 
-      /*  if (event.key == '37') {
-=======
-        if (event.key == 's' || event.key == 'S') {
-            isSPressed = true;
-            console.log(" S true");
+    if (event.key == 's' || event.key == 'S') {
+        isSPressed = true;
+    }
 
-        }
-        if (event.key == '37') {
->>>>>>> e53ff8e2582be527437cb6f8818ce07ee3aedab3
-            isLeftPressed = true;
-            
-            console.log(" left true");
-        }
-        if (event.key == '39') {
-            isRightPressed = true;
-            console.log(" right true");
-
-
-        }*/
-        
-
-
-    });
-    document.onkeydown = function (e) {
-        if (e.keyCode == 37) {
-            isLeftPressed = true;
-
-           // console.log(" left true");
-        }
-        if (e.keyCode == 39) {
-            isRightPressed = true;
-            // console.log(" right true");
-        }
-            if (e.keyCode == 38) {
-                isUpPressed = true;
-
-              //  console.log(" left true");
-            }
-            if (e.keyCode == 40) {
-                isDownPressed = true;
-               // console.log(" right true");
-
-        }
-
+    /*  if (event.key == '37') {
+    =======
+    if (event.key == 's' || event.key == 'S') {
+    isSPressed = true;
+    console.log(" S true");
 
     }
-    document.onkeyup = function (e) {
-        if (e.keyCode == 37) {
-            isLeftPressed = false;
+    if (event.key == '37') {
+    >>>>>>> e53ff8e2582be527437cb6f8818ce07ee3aedab3
+    isLeftPressed = true;
 
-           // console.log(" left true");
-        }
-        if (e.keyCode == 39) {
-            isRightPressed = false;
-           // console.log(" right true");
-
-
-        }
-
-        if (e.keyCode == 38) {
-            isUpPressed = false;
-
-            //  console.log(" left true");
-        }
-        if (e.keyCode == 40) {
-            isDownPressed = false;
-            // console.log(" right true");
-
-        }
-
+    console.log(" left true");
     }
-    /*document.addEventListener("onkeydown", function (event) {
-        console.log(event.keyCode);
-
-        
-        if (event.keyCode == 37) {
-            isLeftPressed = true;
-            
-            console.log(" left true");
-        }
-        if (event.keyCode == 39) {
-            isRightPressed = true;
-            console.log(" right true");
+    if (event.key == '39') {
+    isRightPressed = true;
+    console.log(" right true");
 
 
-        }
+    }*/
 
 
 
-    });*/
+});
 
-    document.addEventListener("keyup", function () {
+document.onkeydown = function (e) {
+    if (e.keyCode == 37) {
+        isLeftPressed = true;
+    }
+    if (e.keyCode == 39) {
+        isRightPressed = true;
+    }
+    if (e.keyCode == 38) {
+        isUpPressed = true;
+    }
+    if (e.keyCode == 40) {
+        isDownPressed = true;
+    }
+}
 
-        if (event.key == 'a' || event.key == 'A') {
-            isAPressed = false;
-           // console.log(" A false");
-        }
-        if (event.key == 'd' || event.key == 'D') {
-            isDPressed = false;
-           //   console.log(" d false");
-        }
-        if (event.key == 'w' || event.key == 'W') {
-            isWPressed = false;
-           //   console.log(" w false");
-        }
-        if (event.key == 's' || event.key == 'S') {
-            isSPressed = false;
-           //   console.log(" w false");
-        }
+document.onkeyup = function (e) {
+    if (e.keyCode == 37) {
+        isLeftPressed = false;
+    }
+    if (e.keyCode == 39) {
+        isRightPressed = false;
+    }
 
-       /*if (event.key == '37') {
-=======
-        if (event.key == 's' || event.key == 'S') {
-            isSPressed = false;
-              console.log(" S false");
-        }
-       if (event.key == '37') {
->>>>>>> e53ff8e2582be527437cb6f8818ce07ee3aedab3
-            isLeftPressed = false;
+    if (e.keyCode == 38) {
+        isUpPressed = false;
+    }
+    if (e.keyCode == 40) {
+        isDownPressed = false;
+    }
+}
 
-            console.log(" left false");
-        }
-        if (event.key == '39') {
-            isRightPressed = false;
+document.addEventListener("keyup", function () {
 
-            console.log(" left false");
-        }*/
-        // breath fire
+    if (event.key == 'a' || event.key == 'A') {
+        isAPressed = false;
+    }
+    if (event.key == 'd' || event.key == 'D') {
+        isDPressed = false;
+    }
+    if (event.key == 'w' || event.key == 'W') {
+        isWPressed = false;
+    }
+    if (event.key == 's' || event.key == 'S') {
+        isSPressed = false;
+    }
 
-       
-    });
+    /*if (event.key == '37') {
+    =======
+    if (event.key == 's' || event.key == 'S') {
+    isSPressed = false;
+    console.log(" S false");
+    }
+    if (event.key == '37') {
+    >>>>>>> e53ff8e2582be527437cb6f8818ce07ee3aedab3
+    isLeftPressed = false;
+
+    console.log(" left false");
+    }
+    if (event.key == '39') {
+    isRightPressed = false;
+
+    console.log(" left false");
+    }*/
+    // breath fire
+
+
+});
 
 
 
 function loadScene() {
-	switch(currentLevel) {
-		case 0:
-	console.log("called level zero!");
-    
-			levelZero();
-			break;
-		case 1:
-			levelOne();
-			break;
-		case 2:
-			levelTwo();
-			break;
-		case 3:
-			levelThree();
-			break;
-		case 4:
-			levelFour();
-			break;
-	}
+    switch(currentLevel) {
+        case 0:
+            levelZero();
+            break;
+        case 1:
+            levelOne();
+            break;
+        case 2:
+            levelTwo();
+            break;
+        case 3:
+            levelThree();
+            break;
+        case 4:
+            levelFour();
+            break;
+        }
 }
 
 function createDragon() {
-	BABYLON.SceneLoader.ImportMesh("", "scenes/", "alduin-dragon.babylon", scene, onDragonLoaded);
+    /*BABYLON.SceneLoader.ImportMesh("", "scenes/", "alduin-dragon.babylon", scene, onDragonLoaded);
 
-	function onDragonLoaded(newMeshes, particleSystems, skeletons) {
-	 	dragon = newMeshes[0];
-	 	dragon.position = new BABYLON.Vector3(0, 30, -1);
-	 	dragon.scaling = new BABYLON.Vector3(0.05, 0.05, 0.05);
-	 	dragon.frontVector = new BABYLON.Vector3(0, 0, -1);
-	 	dragon.rotation.y = 3.14;
+    function onDragonLoaded(newMeshes, particleSystems, skeletons) {
+        dragon = newMeshes[0];
+        dragon.position = new BABYLON.Vector3(0, 30, -1);
+        dragon.scaling = new BABYLON.Vector3(0.05, 0.05, 0.05);
+        dragon.frontVector = new BABYLON.Vector3(0, 0, -1);
+        dragon.rotation.y = 3.14;
         camera.lockedTarget = dragon;
         camera.heightOffset = 20;
         camera.radius = 60;
-    }
+    }*/
+
+    dragon = new BABYLON.Mesh.CreateBox("dragonBox", 2, scene);
+
+    dragon.checkCollisions = true;
+
+    dragon.material = new BABYLON.StandardMaterial("dragonMaterial", scene);
+    dragon.material.diffuseColor = new BABYLON.Color3.Red();
+    dragon.position = new BABYLON.Vector3(0, 20, 500);
+
+    dragon.frontVector = new BABYLON.Vector3(0, 0, -1);
+
+    camera.lockedTarget = dragon;
 }
 
 
-    function applyMovement(){
-    	
-    	if(isAPressed)
-    	{
+function applyMovement(){
 
-    	    dragon.position.x -= 2;
-    	   // console.log("x :" + dragon.position.x)
+    /*if(isAPressed)
+    {
+        dragon.moveWithCollisions(dragon.frontVector.multiplyByFloats(-1, 0, 1));
+    }*/
 
-    	}
-    
-       if (isWPressed) {
-           //  dragon.position.z += -1;
-           //dragon.moveWithCollisions(dragon.frontVector.multiplyByFloats(1, 1, 1));
+    if (isWPressed) {
+        dragon.moveWithCollisions(dragon.frontVector.multiplyByFloats(1, 1, 1));
+    }
 
-           //dragon.frontVector.z--;
-         //  console.log(dragon.frontVector.z * Math.cos(dragon.rotation.y)*-1 );
-          // console.log(dragon.frontVector.z * Math.cos(dragon.rotation.y) * -1);
-           //dragon.position.x +=  dragon.frontVector.x /* * Math.cos(dragon.rotation.y) */ 
-           //dragon.position.z += dragon.frontVector.z /* * Math.cos(dragon.rotation.y)*/  * -1;
-           //console.log("x :" + dragon.position.x + "       Z:" + dragon.position.z)
-          // console.log("Front vector x :" + dragon.frontVector.x + "       Z:" + dragon.frontVector.z)
+    /*if (isDPressed) {
+        var xpos = Math.cos(dragon.rotation.y) * -1;
+        var left = new BABYLON.Vector3(xpos, 0, 0);
+        dragon.moveWithCollisions(left);
+    }*/
 
+    if (isSPressed) {
+        dragon.moveWithCollisions(dragon.frontVector.multiplyByFloats(-1, 1, -1));
+    }
 
-    		//dragon.position.x += 2;
-    	//}
-    	if (isDPressed) {
-    		dragon.position.x += 2;
-    	}
-      /* if (isWPressed) {
-             dragon.position.z += 2;*/
-       }
-       if (isSPressed) {
-             dragon.position.z -= 2;
+    if (isLeftPressed)
+    {
+        dragon.rotation.y -= .1 * 0.4;
+        dragon.frontVector.x  =  Math.sin(dragon.rotation.y) * -1;
+        dragon.frontVector.z  = Math.cos(dragon.rotation.y) * -1;
+    }
 
-       }
-       if (isLeftPressed)
-       {
-           dragon.rotation.y -= .1 * 0.4;
-           dragon.frontVector.x  =  Math.sin(dragon.rotation.y) * -1;
-           dragon.frontVector.z  = Math.cos(dragon.rotation.y) * -1;
-       }
-       if (isRightPressed)
-       {
-           dragon.rotation.y += .1 * 0.4;
-           dragon.frontVector.x = Math.sin(dragon.rotation.y) * -1;
-           dragon.frontVector.z = Math.cos(dragon.rotation.y) * -1;
-       }
-       if (isUpPressed)
-       {
-           dragon.position.y += 1 ;
-       }
-       if (isDownPressed)
-       {
-           dragon.position.y -= 1;
-       }
-       if (dragon.position.x>=500  )
-           dragon.position.x = 500;
-       if (dragon.position.x <= -500)
-           dragon.position.x = -500;
-       if (dragon.position.z >= 500)
-           dragon.position.z = 500;
-       if (dragon.position.z <= -475)
-{
-           dragon.position.z = -475
-     //  dragon.frontVector.x = Math.sin(dragon.rotation.y) * -1;
+    if (isRightPressed)
+    {
+        dragon.rotation.y += .1 * 0.4;
+        dragon.frontVector.x = Math.sin(dragon.rotation.y) * -1;
+        dragon.frontVector.z = Math.cos(dragon.rotation.y) * -1;
+    }
+
+    if (isUpPressed)
+    {
+        dragon.position.y += 1 ;
+    }
+
+    if (isDownPressed)
+    {
+        dragon.position.y -= 1;
+    }
+
+    if (dragon.position.x>=500)
+        dragon.position.x = 500;
+
+    if (dragon.position.x <= -500)
+        dragon.position.x = -500;
+
+    if (dragon.position.z >= 500)
+        dragon.position.z = 500;
+
+    if (dragon.position.z <= -475)
+    {
+        dragon.position.z = -475
+        //  dragon.frontVector.x = Math.sin(dragon.rotation.y) * -1;
         // dragon.frontVector.z = Math.cos(dragon.rotation.y) * -1;
 
-           dragon.position.z = -475;
-}
-
+        dragon.position.z = -475;
     }
+
+}
 
 
 
 function levelZero() {
-	scene = new BABYLON.Scene(engine);
-	currentLevel = 0;
+    scene = new BABYLON.Scene(engine);
+    currentLevel = 0;
 
     //Scene camera
     camera = new BABYLON.FollowCamera("dragonCamera", new BABYLON.Vector3.Zero(), scene);
-    
+
     createDragon();
 
     camera.attachControl(canvas, true);
 
-	//Scene light
-	var light = new BABYLON.HemisphericLight("MainLevelLight", new BABYLON.Vector3(0, 10, 0), scene);
+    //Scene light
+    var light = new BABYLON.HemisphericLight("MainLevelLight", new BABYLON.Vector3(0, 10, 0), scene);
 
     var ground = createConfiguredGround();
 }
 
 function createConfiguredGround()
 {
-
     var ground = new BABYLON.Mesh.CreateGroundFromHeightMap
-        ("ground", "scenes/lake.png", 1000, 1000,
-        50,-50, 100, scene, false, onGroundCreated);
+    ("ground", "scenes/lake.png", 1000, 1000,
+    50,-50, 100, scene, false, onGroundCreated);
 
     var groundMaterial = new BABYLON.StandardMaterial("m1", scene);
     // groundMaterial.ambientColor = new BABYLON.Color3(1, 0, 0);
@@ -345,18 +304,16 @@ function createConfiguredGround()
 
 function loadCoins() {
     BABYLON.SceneLoader.ImportMesh("", "scenes/", "kimoshhh.babylon", scene, onCoinLoaded);
-var zrand;
+    var zrand;
     function onCoinLoaded(newMeshes, particleSystems, skeletons) {
-         for (var i = 0; i < 4; i++) {
-              zrand  = Math.floor(Math.random() * 501);
-              newMeshes[0].position = new BABYLON.Vector3(0, 30, zrand);
-              newMeshes[0].scaling = new BABYLON.Vector3(0.25, 0.25, 0.25);
-              newMeshes[0].material = new BABYLON.StandardMaterial("coinMat", scene);
-              newMeshes[0].material.diffuseColor = new BABYLON.Color3.Yellow();
-              newMeshes[0].rotation.x = Math.PI / 2;
-              coin[i] = newMeshes[0];
-            }   
-
-}
-
+        for (var i = 0; i < 4; i++) {
+            zrand  = Math.floor(Math.random() * 501);
+            newMeshes[0].position = new BABYLON.Vector3(0, 30, zrand);
+            newMeshes[0].scaling = new BABYLON.Vector3(0.25, 0.25, 0.25);
+            newMeshes[0].material = new BABYLON.StandardMaterial("coinMat", scene);
+            newMeshes[0].material.diffuseColor = new BABYLON.Color3.Yellow();
+            newMeshes[0].rotation.x = Math.PI / 2;
+            coin[i] = newMeshes[0];
+        }
+    }
 }
