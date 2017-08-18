76210a065d253d46b0d6fcb9ab2f03aec873c223
yehia67 <yehiatarek67@gmail.com>
2017-08-18 17:39:11 +0200

    add shotting star to the game. need to clone them & modify movement
    Former-commit-id: 1c65116d93036154881c0d1d18e440a05fe036fb
    Former-commit-id: 1dbb23346d1b6688c78208868128fb33b41c7a84
    Former-commit-id: 67b018822e48e87f2e34d3422f5a0c6bea193992

--- js/game.js
+++ js/game.js
@@ -31,15 +31,16 @@ function startGame() {
     loadScene();
     scene.collisionsEnabled = true;
 
-    var skybox = BABYLON.Mesh.CreateBox("skyBox", 10000.0, scene);
-    var skyboxMaterial = new BABYLON.StandardMaterial("skyBox", scene);
-    skyboxMaterial.backFaceCulling = false;
-    skyboxMaterial.disableLighting = true;
-    skybox.material = skyboxMaterial;
-    skybox.infiniteDistance = true;
-    skyboxMaterial.reflectionTexture = new BABYLON.CubeTexture("scenes/sky/sky", scene);
-    skyboxMaterial.reflectionTexture.coordinatesMode = BABYLON.Texture.SKYBOX_MODE;
-
+    //var skybox = BABYLON.Mesh.CreateBox("skyBox", 10000.0, scene);
+    //var skyboxMaterial = new BABYLON.StandardMaterial("skyBox", scene);
+    //skyboxMaterial.backFaceCulling = false;
+    //skyboxMaterial.disableLighting = true;
+    //skybox.material = skyboxMaterial;
+    //skybox.infiniteDistance = true;
+    //skyboxMaterial.reflectionTexture = new BABYLON.CubeTexture("scenes/sky/sky", scene);
+   // skyboxMaterial.reflectionTexture.coordinatesMode = BABYLON.Texture.SKYBOX_MODE;
+    createDragon();
+    createRocks();
     engine.runRenderLoop(function() {
         if(scene) {
             scene.render();
@@ -183,18 +184,18 @@ function loadScene() {
 }
 
 function createDragon() {
-    /*BABYLON.SceneLoader.ImportMesh("", "scenes/", "alduin-dragon.babylon", scene, onDragonLoaded);
+    BABYLON.SceneLoader.ImportMesh("", "scenes/", "BGE_Dragon_2.5_Blender_Game_Engine.blend.babylon", scene, onDragonLoaded);
 
     function onDragonLoaded(newMeshes, particleSystems, skeletons) {
         dragon = newMeshes[0];
         dragon.position = new BABYLON.Vector3(0, 30, -1);
-        dragon.scaling = new BABYLON.Vector3(0.05, 0.05, 0.05);
+        dragon.scaling = new BABYLON.Vector3(0.51, 0.51, 0.51);
         dragon.frontVector = new BABYLON.Vector3(0, 0, -1);
         dragon.rotation.y = 3.14;
         camera.lockedTarget = dragon;
         camera.heightOffset = 20;
         camera.radius = 60;
-    }*/
+    }
 
     dragon = new BABYLON.Mesh.CreateBox("dragonBox", 2, scene);
 
@@ -209,6 +210,71 @@ function createDragon() {
     dragon.frontVector = new BABYLON.Vector3(0, 0, -1);
 
     camera.lockedTarget = dragon;
+}
+function createRocks(){
+//var sphere = BABYLON.Mesh.CreateSphere("sphere", 10.0, 10.0, scene);
+    var fountain = BABYLON.Mesh.CreateSphere("foutain", 10.0, scene);
+    var smokeSystem = new BABYLON.ParticleSystem("particles", 1000, scene);
+    smokeSystem.particleTexture = new BABYLON.Texture("textures/flare.png", scene);
+    smokeSystem.emitter = fountain; // the starting object, the emitter
+    smokeSystem.minEmitBox = new BABYLON.Vector3(-0.5, 1, -0.5); // Starting all from
+    smokeSystem.maxEmitBox = new BABYLON.Vector3(2, 1, 2); // To...
+    smokeSystem.color1 = new BABYLON.Color4(0.1, 0.1, 0.1, 1.0);
+    smokeSystem.color2 = new BABYLON.Color4(0.1, 0.1, 0.1, 1.0);
+    smokeSystem.colorDead = new BABYLON.Color4(0, 0, 0, 0.0);
+    smokeSystem.minSize = 0.3;
+    smokeSystem.maxSize = 5;
+    smokeSystem.minLifeTime = 0.3;
+    smokeSystem.maxLifeTime = 1.5;
+    smokeSystem.emitRate = 350;
+// Blend mode : BLENDMODE_ONEONE, or BLENDMODE_STANDARD
+   smokeSystem.blendMode = BABYLON.ParticleSystem.BLENDMODE_ONEONE;
+   smokeSystem.gravity = new BABYLON.Vector3(0, 0, 0);
+   smokeSystem.direction1 = new BABYLON.Vector3(-1.5, 8, -1.5);
+   smokeSystem.direction2 = new BABYLON.Vector3(1.5, 8, 1.5);
+   smokeSystem.minAngularSpeed = 0;
+   smokeSystem.maxAngularSpeed = Math.PI;
+   smokeSystem.minEmitPower = 0.5;
+   smokeSystem.maxEmitPower = 1.5;
+   smokeSystem.updateSpeed = 0.005;
+   smokeSystem.start();
+     // Create a particle system
+    var fireSystem = new BABYLON.ParticleSystem("particles", 2000, scene);
+    //Texture of each particle
+    fireSystem.particleTexture = new BABYLON.Texture("textures/flare.png", scene);
+    // Where the particles come from
+    fireSystem.emitter = fountain; // the starting object, the emitter
+    fireSystem.minEmitBox = new BABYLON.Vector3(-0.5, 1, -0.5); // Starting all from
+    fireSystem.maxEmitBox = new BABYLON.Vector3(2, 1, 2); // To...
+    // Colors of all particles
+    fireSystem.color1 = new BABYLON.Color4(1, 0.5, 0, 1.0);
+    fireSystem.color2 = new BABYLON.Color4(1, 0.5, 0, 1.0);
+    fireSystem.colorDead = new BABYLON.Color4(0, 0, 0, 0.0);
+     // Size of each particle (random between...
+    fireSystem.minSize = 0.3;
+    fireSystem.maxSize = 5;    
+    // Life time of each particle (random between...
+    fireSystem.minLifeTime = 0.2;
+    fireSystem.maxLifeTime = 0.4;
+    // Emission rate
+    fireSystem.emitRate = 600;
+   // Blend mode : BLENDMODE_ONEONE, or BLENDMODE_STANDARD
+    fireSystem.blendMode = BABYLON.ParticleSystem.BLENDMODE_ONEONE;
+    // Set the gravity of all particles
+    fireSystem.gravity = new BABYLON.Vector3(0, 0, 0);
+    // Direction of each particle after it has been emitted
+    fireSystem.direction1 = new BABYLON.Vector3(0, 4, 0);
+    fireSystem.direction2 = new BABYLON.Vector3(0, 4, 0);
+    // Angular speed, in radians
+    fireSystem.minAngularSpeed = 0;
+    fireSystem.maxAngularSpeed = Math.PI;
+    // Speed
+    fireSystem.minEmitPower = 1;
+    fireSystem.maxEmitPower = 3;
+    fireSystem.updateSpeed = 0.007;
+    // Start the particle system
+    fireSystem.start();         
+
 }
 
 
