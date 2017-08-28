cf79ba598f9b59f82094ba70fd993ccca11c38a7
yehia67 <yehiatarek67@gmail.com>
2017-08-28 21:39:38 +0200

    add door but have some bugs
    Former-commit-id: f210b81ebce5acc6399bc4a5a097b9db37ef7190
    Former-commit-id: f5184c97b10d1c43fd3aacbe20bce9de71e294d4
    Former-commit-id: 6fccc70835d7da04ebaa0abcab141b20b2a52867

--- js/game.js
+++ js/game.js
@@ -2,7 +2,7 @@
 /// <reference path="cannon.max.js" />
 
 document.addEventListener("DOMContentLoaded", startGame, false);
-
+var door;
 var canvas;
 var engine;
 var scene;
@@ -57,6 +57,10 @@ function startGame() {
     engine.runRenderLoop(function() {
         if(scene) {
             scene.render();
+            if(score > 2)
+            {
+                door.isVisible = true;
+            }
             if(dragon) {
                 applyMovement();
                 if(!meteorFlag) {
@@ -680,7 +684,7 @@ function levelZero() {
 
     loadCoins(10);
     createDragon();
-   
+   createDoor();
 }
 
 function createConfiguredGround()
@@ -873,4 +877,22 @@ function calculateBoundingBoxOfCompositeMeshes(newMeshes, flag) {
 
     return { min: { x: minx, y: miny, z: minz }, max: { x: maxx, y: maxy, z: maxz }, lengthX: _lengthX, lengthY: _lengthY, lengthZ: _lengthZ, center: _center, boxMesh: _boxMesh };
 
+}
+function createDoor() {
+    BABYLON.SceneLoader.ImportMesh("", "scenes/", "low-poly-wooden-door.babylon", scene, onDoorLoaded);
+     console.log("create door");
+    function onDoorLoaded(newMeshes, particleSystems, skeletons) {
+        door = newMeshes[0];
+        door.position = new BABYLON.Vector3(0, 70, 30);
+        door.scaling = new BABYLON.Vector3(51, 51, 51);
+        door.frontVector = new BABYLON.Vector3(0, 0, -1);
+        door.rotation.y = 3.14;
+        door.checkCollisions = true;
+        door.isVisible = false;
+        if(door.intersectsMesh(dragon,false))
+        {
+            console.log("next level");
+        }
+      
+    }
 }
