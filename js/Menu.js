93a57f24967bb4cee9ba59a303aef94ffe378b7a
MahmoudKhalil <mahmoudkhalil11@gmail.com>
2017-09-06 11:41:46 +0200

    Delete Menu.js
    Former-commit-id: 8563a518404b9240ebec24d2314fe2b7a2f58487
    Former-commit-id: a74535e1af85febcc649ebaa03c55e8641197ebb
    Former-commit-id: be00acd98fa10fe8dc4def71e54a820aeadb6b6a

--- js/Menu.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", startGame1, false);
-
-
-function startGame1() {
-	console.log("heeeeere");
-	document.getElementById("myBtn").addEventListener("click", function(){
-		MainGame();
-	});
-}
